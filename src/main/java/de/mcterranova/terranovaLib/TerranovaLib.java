b94f6a9d6dc75b01b0db00d003581a562662c2f5
derbasti <s.tuetti@gmx.de>
2024-08-28 18:52:33 +0200

    Initial

---
+++ src/main/java/de/mcterranova/terranovaLib/TerranovaLib.java
@@ -0,0 +1,17 @@
+package de.mcterranova.terranovaLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TerranovaLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
