52d432845b5fb7f46aeedaeffe4752b2af9f54ae
VonNekyia <germarten@gmail.com>
2024-09-25 18:18:37 +0200

    added Craft listener class

--- src/main/java/de/mcterranova/terranovaLib/TerranovaLib.java
+++ src/main/java/de/mcterranova/terranovaLib/TerranovaLib.java
@@ -19,6 +19,7 @@ public final class TerranovaLib extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         saveDefaultConfig();
         FileConfiguration config = getConfig();
 
---
+++ src/main/java/de/mcterranova/terranovaLib/roseGUI/listener/RoseCraftListener.java
@@ -0,0 +1,11 @@
+package de.mcterranova.terranovaLib.roseGUI.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class RoseCraftListener implements Listener {
+
+    //@EventHandler
+
+
+}
